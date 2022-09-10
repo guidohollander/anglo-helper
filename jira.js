e2dd1beb38a893f0cb039ab2062c61931741455e
guidohollander <guido.hollander@gmail.com>
2022-09-10 09:43:58 +0200

    updated

--- jira.js
+++ jira.js
@@ -4,18 +4,77 @@ var apiTopic = 'issue';
 var jira; //the jira api
 const axios = require('axios');
 
-function jira_init(username, password, baseURL) {
-  baseURL = baseURL;
-  var JiraApi = require('jira-client');
-  jira = new JiraApi({
-    protocol: 'https',
-    host: 'jira.bearingpointcaribbean.com',
-    username: username,
-    password: password,
-    apiVersion: '2',
-    strictSSL: true
+function updateJiraIssueFixVersion(jiraIssueNumber, fixVersion) {
+  var request = require('request');
+  var Cookie = require('request-cookies').Cookie;
+  request.get(`${baseURL}/${restApi}/${apiTopic}/${jiraIssueNumber}`, function (err, response, body) {
+    var rawcookies = response.headers['set-cookie'];
+    for (var i in rawcookies) {
+      var cookie = new Cookie(rawcookies[i]);
+      var options = {
+        'method': 'PUT',
+        'url': `${baseURL}${restApi}/${apiTopic}/${jiraIssueNumber}`,
+        'headers': {
+          'Authorization': 'Basic ' + Buffer.from(username + ":" + password, 'binary').toString('base64'),
+          'Content-Type': 'application/json',
+          'Cookie': cookie
+        },
+        body: JSON.stringify({ "update": { "fixVersions": [{ "set": [{ "name": fixVersion }] }] } })
+      };
+      request(options, function (error, response) {
+        if (error) throw new Error(error);
+        console.log(response.body);
+      });
+    }
   });
 }
+
+async function getJiraIssue(jiraUsername, jiraPassword, jiraIssueNumber) {
+  return await jiraGet(jiraUsername, jiraPassword, `https://jira.bearingpointcaribbean.com/rest/api/latest/issue/${jiraIssueNumber}`)  
+}
+
+const jiraGet = async (username, password, url) => {
+  try {
+    const resp = await axios.get(url, {
+      'headers': {
+        'Authorization': 'Basic ' + Buffer.from(username + ":" + password, 'binary').toString('base64'),
+        'Content-Type': 'application/json'
+      }
+    }, {
+      auth: {
+        username: username,
+        password: password
+      }
+    });
+    //console.log(resp.data);
+    return resp.data
+  } catch (err) {
+    // Handle Error Here
+    console.error(err);
+  }
+};
+
+const jiraPost = async (username, password, url, data) => {
+  try {
+
+    var config = {
+      method: 'post',
+      url: 'https://jira.bearingpointcaribbean.com/rest/api/latest/version',
+      headers: { 
+        'Authorization': 'Basic ' + Buffer.from(username + ":" + password, 'binary').toString('base64'),
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+
+    const resp = await axios(config);
+    return resp.data
+  } catch (err) {
+    // Handle Error Here
+    console.error(err);
+  }
+};
+
 
 function updateJiraIssueFixVersion(jiraIssueNumber, fixVersion) {
   var request = require('request');
@@ -42,34 +101,32 @@ function updateJiraIssueFixVersion(jiraIssueNumber, fixVersion) {
   });
 }
 
-async function getJiraIssue(jiraIssueNumber) {
-  return await jira.findIssue(jiraIssueNumber)
+// intend: create a new version for a particular project, if necessary
+async function addVersionIfNotExists(jiraUsername, jiraPassword, project, versionToAdd) {
+    //get all versions of project and check if name already exists
+    versions = await jiraGet(jiraUsername, jiraPassword, `https://jira.bearingpointcaribbean.com/rest/api/latest/project/${project}/versions`)
+    //check if project fixverison exists
+    if (versions.findIndex(element => (element.name === versionToAdd)) === -1) {
+        //15501 = mbsai
+        var data = JSON.stringify({
+            "archived": false,
+            "releaseDate": new Date().toISOString().replaceAll('T', '').replaceAll(':', '').substring(0, 10),
+            "name": versionToAdd,
+            "projectId": versions[0].projectId,
+            "released": false
+        });
+        var result = await jiraPost(jiraUsername, jiraPassword, 'https://jira.bearingpointcaribbean.com/rest/api/latest/version', data);
+        if (result.self) {
+            console.log('Added', result.self);
+            return result.self
+        }
+    }
 }
 
-const jiraGet = async (username, password, url) => {
-  try {
-    const resp = await axios.get(url, {
-      'headers': {
-        'Authorization': 'Basic ' + Buffer.from(username + ":" + password, 'binary').toString('base64'),
-        'Content-Type': 'application/json'
-      }
-    }, {
-      auth: {
-        username: username,
-        password: password
-      }
-    });
-    //console.log(resp.data);
-    return resp.data
-  } catch (err) {
-    // Handle Error Here
-    console.error(err);
-  }
-};
-
 module.exports = {
-  jira_init: jira_init,
   updateJiraIssueFixVersion: updateJiraIssueFixVersion,
   getJiraIssue: getJiraIssue,
-  jiraGet: jiraGet
+  jiraGet: jiraGet,
+  jiraPost : jiraPost,
+  addVersionIfNotExists : addVersionIfNotExists
 }
